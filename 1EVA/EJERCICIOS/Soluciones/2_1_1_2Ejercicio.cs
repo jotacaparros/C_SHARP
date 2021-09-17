fde0c7f84621c4d3a428fa63955dfc38f3145eaa
mañana <jotacaparros@gmail.com>
2021-09-17 12:33:31 +0200

    17/09/2021

--- 1EVA/EJERCICIOS/Soluciones/2_1_1_2Ejercicio.cs
+++ 1EVA/EJERCICIOS/Soluciones/2_1_1_2Ejercicio.cs
@@ -5,7 +5,7 @@ public class Ejercicio_2_1_1_2
 	/* (2.1.1.2) Crea un programa que pida al usuario dos números enteros y diga cuál es
 	el mayor de ellos. */
 
-	public static void xxxxx()
+	public static void xxxx()
 	{
 		int numUsuario1, numUsuario2;
 
@@ -14,13 +14,22 @@ public class Ejercicio_2_1_1_2
 		numUsuario1 = Convert.ToInt32(Console.ReadLine());
 		numUsuario2 = Convert.ToInt32(Console.ReadLine());
 
-		if (numUsuario1 > numUsuario2)
-        {
-			Console.WriteLine("{0} es mayor que {1}", numUsuario1, numUsuario2);
-        }
+		if (numUsuario1 == numUsuario2)
+		{
+			Console.WriteLine("{0} es igual que {1}", numUsuario2, numUsuario1);
+		}
+
+		else
+		{
+			if (numUsuario1 > numUsuario2)
+			{
+				Console.WriteLine("{0} es mayor que {1}", numUsuario1, numUsuario2);
+			}
 			else
 			{
 				Console.WriteLine("{0} es mayor que {1}", numUsuario2, numUsuario1);
 			}
+		}		
+		
 	}
 }
---
+++ 1EVA/EJERCICIOS/Soluciones/2_1_1_3Ejercicio.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Ejercicio_2_1_1_3
+{
+	/*(2.1.1.3) Crea un programa que pida al usuario dos números enteros y diga si el
+	primero es múltiplo del segundo (pista: igual que antes, habrá que ver si el resto
+	de la división es cero: a % b == 0). */
+
+	public static void Main()
+	{
+
+
+	}
+}
