fc60dc4e864d86a8da9dab3e99c27edf6f348c1e
mañana <jotacaparros@gmail.com>
2021-09-20 13:00:41 +0200

    20/09/21

---
+++ 1EVA/EJERCICIOS/Soluciones/2_1_9_1Ejercicio.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Ejercicio_2_1_9_1
+{
+	/* Crea un programa que pida un número del 1 al 5 al usuario, y escriba el
+	nombre de ese número, usando "switch" (por ejemplo, si introduce "1", el
+	programa escribirá "uno"). */
+	public static void xxxx()
+	{
+		Console.WriteLine("Dame un número y te lo escribo");
+
+		int num1;
+
+	num1 = Convert.ToInt32(Console.ReadLine());
+
+		switch (num1)
+        {
+			case 1: Console.WriteLine("Uno");
+				break;
+			case 2:Console.WriteLine("Dos");
+				break;
+			case 3:
+				Console.WriteLine("tres");
+				break;
+			case 4:
+				Console.WriteLine("Cuatro");
+				break;
+			case 5:
+				Console.WriteLine("Cinco");
+				break;
+			default:
+				Console.WriteLine("Número muy grande");
+				break;
+
+
+		}
+	}
+}
