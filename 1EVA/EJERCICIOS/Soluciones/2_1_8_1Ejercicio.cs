402f1a7c19f673d7bd1d6388af90a7cb34576d66
mañana <jotacaparros@gmail.com>
2021-09-20 12:29:06 +0200

    20/09/21

---
+++ 1EVA/EJERCICIOS/Soluciones/2_1_8_1Ejercicio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Ejercicio_2_1_8_1
+{
+	/*(2.1.8.1) Crea un programa que use el operador condicional para mostrar un el
+	valor absoluto de un número de la siguiente forma: si el número es positivo, se
+	mostrará tal cual; si es negativo, se mostrará cambiado de signo.*/
+	
+	public static void xxxx()
+	{
+		Console.WriteLine("Echame un número a la cara. Puede ser positivo o negativo");
+
+		int num1 = 0, resultado;
+
+		num1 = Convert.ToInt32(Console.ReadLine());
+
+		resultado = num1 > 0 ? num1 : num1 * -1;
+
+		Console.WriteLine("El resultado es {0}", resultado);
+	}
+}
---
+++ 1EVA/EJERCICIOS/Soluciones/2_1_8_2Ejercicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Ejercicio_2_1_8_2
+{
+	/*(2.1.8.2) Usa el operador condicional para calcular el menor de dos números */
+
+	public static void xxx()
+	{
+		Console.WriteLine("Dame dos números y te digo cual es el mayor");
+		int num1, num2, resultado;
+
+		num1 = Convert.ToInt32(Console.ReadLine());
+		num2 = Convert.ToInt32(Console.ReadLine());
+
+		resultado = num1 > num2 ? num1 : num2;
+
+		Console.WriteLine("{0} es mayor", resultado);
+	}
+}
