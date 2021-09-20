0aee09b0d1b096fd8fc2636c96fa6a40d6d777f0
mañana <jotacaparros@gmail.com>
2021-09-20 11:53:38 +0200

    17/09/2021

---
+++ 1EVA/EJERCICIOS/Soluciones/2_1_5_1Ejercicio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Ejercicio_2_1_5_1
+{
+	/*(2.1.5.1) Crea un programa que pida al usuario un número entero y 
+	responda si es múltiplo de 2 o de 3. */
+
+	public static void XXX()
+	{
+		int num1 = 0;
+
+		Console.WriteLine("Dame un número");
+
+		num1 = Convert.ToInt32(Console.ReadLine());
+
+		if (num1 % 2 != 0 && num1 % 3 != 0)
+		{
+			Console.WriteLine("Tu número no es múltiplo de 2 ni de 3");
+		}
+			else
+			{
+				if (num1 % 2 == 0 && num1 % 3 == 0)
+				{
+				Console.WriteLine("Tu número es múltiplo de 2 y de 3");
+				}
+
+					else
+					{
+						if (num1 % 3 == 0)
+						{
+						Console.WriteLine("Tu número es múltiplo de 3");
+						}
+							else
+							{
+								if (num1 % 2 == 0)
+								{
+								Console.WriteLine("Tu número es múltiplo de 2");
+								}
+							}
+					}
+			}
+	}
+}
---
+++ 1EVA/EJERCICIOS/Soluciones/2_1_5_5Ejercicio.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Ejercicio_2_1_5_5
+{
+	/*(2.1.5.5) Crea un programa que pida al usuario dos números enteros y diga si
+	ambos son pares.*/
+
+	public static void Main()
+	{
+		int num1 = 0, num2 = 0;
+
+		Console.WriteLine("Dame 2 números");
+
+		num1 = Convert.ToInt32(Console.ReadLine());
+		num2 = Convert.ToInt32(Console.ReadLine());
+
+		if ( num1 % 2 == 0 && num2 % 2 == 0)
+        {
+			Console.WriteLine("Tus 2 números son pares");
+		}
+			else
+			{
+				if (num1 % 2 != 0 && num2 % 2 != 0)
+				{
+				Console.WriteLine("Tus 2 números son impares");
+				}
+					else
+					{
+						if (num1 % 2 == 0 && num2 % 2 != 0)
+						{
+						Console.WriteLine("Tu primer número es par y tu segundo es impar");
+						}
+							else
+							{
+								if (num1 % 2 != 0 && num2 % 2 == 0)
+								{
+									Console.WriteLine("Tu primer número es impar y tu segundo es par");
+								}
+							}
+					}
+			}
+
+	}
+}
