2b2bddf340d1f87bb84be83da4591b992739156a
mañana <jotacaparros@gmail.com>
2021-09-24 12:43:49 +0200

    24/09/21 Bucles - While

---
+++ 1EVA/EJERCICIOS/Soluciones/2_2_1_2_1Ejercicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Ejercicio_2_2_1_2_1
+{
+
+	/*(2.2.1.2.1) Crea un programa que escriba en pantalla los números del 1 al 10,
+	usando "while" */
+	public static void xxx()
+	{
+		int num = 0;
+		
+		while (num < 10)
+        {
+			num = num + 1;
+			Console.WriteLine(num);
+        }
+
+	}
+}
