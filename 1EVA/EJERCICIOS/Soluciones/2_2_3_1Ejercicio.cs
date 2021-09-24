57d2121bc5bdf7c85b077611ad87b0615cca8c90
mañana <jotacaparros@gmail.com>
2021-09-24 13:55:48 +0200

    24/09/21 Bucles - For

--- 1EVA/EJERCICIOS/Soluciones/2_2_3_1Ejercicio.cs
+++ 1EVA/EJERCICIOS/Soluciones/2_2_3_1Ejercicio.cs
@@ -5,7 +5,7 @@ public class Ejercicio_2_2_3_1
 	 /*(2.2.3.1) Crea un programa que muestre los números del 10 al 20, ambos
 	incluidos.*/
 
-	public static void Main()
+	public static void xxx()
 	{
 		int num;
 
