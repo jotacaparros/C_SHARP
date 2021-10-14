2620e385e1d2f79bf48ae7d9d79d073fe73d83c3
mañana <jotacaparros@gmail.com>
2021-10-14 10:59:08 +0200

    14/10/21

--- 1EVA/EJERCICIOS/Soluciones/2_6_7Ejercicio.cs
+++ 1EVA/EJERCICIOS/Soluciones/2_6_7Ejercicio.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-namespace Soluciones
-{
     public class Ejercicio_2_6_7
     {
         /*(2.6.7) Crea un programa que "dibuje" un triángulo creciente, alineado a la
@@ -11,7 +9,7 @@ namespace Soluciones
           *** 
          **** 
                 */
-        public static void Main()
+        public static void EjercicioCabron()
         {
             int alt, anch;
 
@@ -19,14 +17,28 @@ namespace Soluciones
             anch = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Alto:");
             alt = Convert.ToInt32(Console.ReadLine());
+            
 
             for (int i = 0; i < alt; i++)
             {
                 for (int j = 0; j < anch; j++)
                 {
-
+                    // Momento crítico que me ha costado pillar:
+                        //si j (el contador del ancho del triangulo) es menor que 
+                        //el ancho menos i (el contador del altor del triangulo)
+                    if ( j < (anch - i))
+                    {
+                        //escribe espacios en blanco
+                        Console.Write(" ");
+                    }
+                        //Y si no escribe *
+                        else
+                        {
+                           Console.Write("*");
+                        }
                 }
+                Console.WriteLine();
             }
         }
     }
-}
+
