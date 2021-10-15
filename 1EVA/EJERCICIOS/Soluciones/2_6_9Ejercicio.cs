a11337a33fb28dd3a743d3d2d6e263eb42458613
mañana <jotacaparros@gmail.com>
2021-10-15 13:55:29 +0200

    15/10/21

---
+++ 1EVA/EJERCICIOS/Soluciones/2_6_9Ejercicio.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Ejercicio_2_6_9
+{
+	public static void Main()
+	{
+		/*(2.6.9) Crea un programa que "dibuje" un cuadrado formado por cifras sucesivas, 
+		con el tamaño que indique el usuario, hasta un máximo de 9. Por ejemplo, si desea 
+		tamaño 5, el cuadrado sería así:
+		11111
+		22222
+		33333
+		44444
+		55555 */
+
+		Console.Write("Del 1 al 9 ¿Cómo de grande lo quieres?");
+		int num;
+
+        for (int i = 0; i < num; i++)
+        {
+            for (int j = 0; j < num; j++)
+            {
+				Console.Write(i);
+
+            }
+
+        }
+
+	}
+
+		
+}
