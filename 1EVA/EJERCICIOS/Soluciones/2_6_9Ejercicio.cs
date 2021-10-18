3b84bbeb08d4b930cb9aefebd4758160ac5f8620
mañana <jotacaparros@gmail.com>
2021-10-18 10:33:44 +0200

    18/10/21

--- 1EVA/EJERCICIOS/Soluciones/2_6_9Ejercicio.cs
+++ 1EVA/EJERCICIOS/Soluciones/2_6_9Ejercicio.cs
@@ -2,7 +2,7 @@
 
 public class Ejercicio_2_6_9
 {
-	public static void Main()
+	public static void MuyGrande()
 	{
 		/*(2.6.9) Crea un programa que "dibuje" un cuadrado formado por cifras sucesivas, 
 		con el tamaño que indique el usuario, hasta un máximo de 9. Por ejemplo, si desea 
@@ -13,17 +13,18 @@ public class Ejercicio_2_6_9
 		44444
 		55555 */
 
-		Console.Write("Del 1 al 9 ¿Cómo de grande lo quieres?");
+		Console.WriteLine("Del 1 al 9 ¿Cómo de grande lo quieres?");
 		int num;
+		num = Convert.ToInt32(Console.ReadLine());
 
         for (int i = 0; i < num; i++)
         {
             for (int j = 0; j < num; j++)
             {
-				Console.Write(i);
+				Console.Write(i + 1);
 
             }
-
+			Console.WriteLine();
         }
 
 	}
