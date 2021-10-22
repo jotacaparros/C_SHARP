4894f43edc3bfb98dcb021397bbcb7c83471c85b
mañana <jotacaparros@gmail.com>
2021-10-22 13:55:10 +0200

    22/10/21 - Calculadora

--- 1EVA/EJERCICIOS/Practica_1_1/Practica_1_1.cs
+++ 1EVA/EJERCICIOS/Practica_1_1/Practica_1_1.cs
@@ -12,62 +12,107 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            int numA, numB = 0, solucion = 0;
-            char opera;
+            int numA, numB = 0, numC = 0, solucion = 0;
+            char opera, salida;
 
             Console.WriteLine("CALCULADORA");
 
             do
             {
-                numero:
+                numero:    
                 Console.WriteLine("INTRODUCE UN NÚMERO:");
                 numA = Convert.ToInt32(Console.ReadLine());
 
+                otra:
                 Console.WriteLine("INTRODUCE UNA OPERACIÓN:");
                 opera = Convert.ToChar(Console.ReadLine());
-                    if (opera == '=')
+                if (opera == '=')
+                {
+                    goto operacion;
+                }
+                    else
                     {
-                        goto operacion;
+                        if ((opera != '+') && (opera != '-') && (opera != '*') && (opera != '/'))
+                        {
+                            goto operacion;
+                        }
                     }
 
                 Console.WriteLine("INTRODUCE UN NÚMERO:");
                 numB = Convert.ToInt32(Console.ReadLine());
 
-            operacion:
+                operacion:
                 switch (opera)
                 {
                     case '=':
                         Console.WriteLine(solucion);
                         goto numero;
                     case '+':
-                        solucion = solucion + (numA + numB);
+                        solucion = (numA + numB);
                         break;
                     case '-':
-                        solucion = solucion - (numA - numB);
+                        solucion = (numA - numB);
                         break;
                     case '/':
-                        solucion = solucion / (numA / numB);
+                        solucion = (numA / numB);
                         break;
                     case '*':
-                        solucion = solucion * (numA * numB);
+                        solucion = (numA * numB);
                         break;
                     default:
                         Console.WriteLine("INTRODUCE UN CARÁCTER VALIDO");
+                        goto otra;
                         break;
 
                 }
                 
-
+                operacion2:
                 Console.WriteLine("INTRODUCE UNA OPERACIÓN:");
                 opera = Convert.ToChar(Console.ReadLine());
                     if(opera=='=')
                     {
                         goto operacion;
                     }
+                    else 
+                    {
+                        if ((opera != '+') && (opera != '-') && (opera != '*') && (opera != '/'))
+                        {
+                        goto operacion;
+                        }
+                            else
+                            {
+                                Console.WriteLine("INTRODUCE UN NÚMERO:");
+                                numC = Convert.ToInt32(Console.ReadLine());
+                                switch (opera)
+                                {
+                                    case '=':
+                                        Console.Write(solucion);
+                                        goto numero;
+                                    case '+':
+                                        solucion = solucion + numC;
+                                        break;
+                                    case '-':
+                                        solucion = solucion - numC;
+                                        break;
+                                    case '/':
+                                        solucion = solucion / numC;
+                                        break;
+                                    case '*':
+                                        solucion = solucion * numC;
+                                        break;
+                                    default:
+                                        Console.WriteLine("INTRODUCE UN CARÁCTER VALIDO");
+                                        break;
+
+                                }
+                            }                  
+                        goto operacion2;
+                    }
+                
 
               
             }
-            while (numA != 's' || /* numB != 's' ||*/ opera != 's' /*opera2 != 's'*/);
+            while (salida != 's');
 
             Console.WriteLine("FIN DEL PROGRAMA");
         }
