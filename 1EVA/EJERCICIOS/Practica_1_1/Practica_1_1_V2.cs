30c2eb831dd6a4aa2aa8903ecaa14d3246faea64
Jota Caparrós <jotacaparros@gmail.com>
2021-11-02 21:54:35 +0100

    2/11/21

--- 1EVA/EJERCICIOS/Practica_1_1/Practica_1_1_V2.cs
+++ 1EVA/EJERCICIOS/Practica_1_1/Practica_1_1_V2.cs
@@ -4,54 +4,127 @@ public class Practica_1_1_V2
 {
 	public static void Main()
 	{
-		int numero = 0;
+		float numero = 0, solucion = 0;
 		char opera;
-		bool condicion = true;
+		bool esNumeroValido = true, esOperacionValida = false;
 		do
 		{
 			do
 			{
-				try
+                try
 				{
-					numero = Convert.ToInt32(Console.ReadLine());
-					condicion = false;
+					Console.Write("Introduce un número: ");
+					numero = float.Parse(Console.ReadLine());
+                    solucion = numero;
+					esNumeroValido = true;
 				}	
-					catch(Exception)
-					{
-						condicion = true;
-					}
-
-			} while (condicion == true);
-
-			do
-			{
-				opera = Convert.ToChar(Console.ReadLine());
-
-				switch (opera)
-				{
-					case '+':
-						numero += numero;
-						break;
-					case '-':
-						numero -= numero;
-						break;
-					case '*':
-						numero *= numero;
-						break;
-					case '/':
-						if (numero != 0)
-						{
-							numero /= numero;
-						}
-						else
-						{
-							Console.WriteLine("No se puede dividir un número entre 0");
-						}
-						break;
-					default:
+				catch(Exception)
+				{	
+					Console.WriteLine("--> Número no valido");
 				}
 
-			} while (opera != 's' || opera != '=');
+			} while (!esNumeroValido);
+
+            esOperacionValida = false;
+
+            do
+            {
+
+               Console.Write("Dame un operador: ");
+               opera = Convert.ToChar(Console.ReadLine());
+
+               switch (opera)
+               {
+                    case '+':
+                        do
+                        {
+                            try
+                            {
+                                Console.Write("Introduce un número: ");
+                                numero = float.Parse(Console.ReadLine());
+                                esNumeroValido = true;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("--> Número no valido");
+                            }
+
+                        } while (!esNumeroValido);
+                        solucion += numero;
+                        break;
+
+                    case '-':
+                        do
+                        {
+                            try
+                            {
+                                Console.Write("Introduce un número: ");
+                                numero = float.Parse(Console.ReadLine());
+                                esNumeroValido = true;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("--> Número no valido");
+                            }
+
+                        } while (!esNumeroValido);
+                        solucion -= numero;
+                        break;
+
+                    case '*':
+                        do
+                        {
+                            try
+                            {
+                                Console.Write("Introduce un número: ");
+                                numero = float.Parse(Console.ReadLine());
+                                esNumeroValido = true;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("--> Número no valido");
+                            }
+
+                        } while (!esNumeroValido);
+                        solucion *= numero;
+                        break;
+
+                    case '/':
+                        do
+                        {
+                            try
+                            {
+                                Console.Write("Introduce un número: ");
+                                numero = float.Parse(Console.ReadLine());
+                                esNumeroValido = true;
+                            }
+                            catch (Exception)
+                            {
+                                Console.WriteLine("--> Número no valido");
+                            }
+
+                        } while (!esNumeroValido);
+                        try
+                        {
+                            solucion /= numero;  
+                        }
+                        catch (DivideByZeroException)
+                        {
+                            Console.WriteLine("No se puede dividir un número entre 0");
+                        }
+                        break;
+
+                    case '=':
+                        esOperacionValida = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Operador no valido");
+                        break;
+               }        
+            } while (!esOperacionValida);
+
+            Console.WriteLine("El resultado es {0}", solucion);
 
         } while(opera != 's');
 
