0434be018e0932d3f2c70d0c65ba6821e35821ae
mañana <jotacaparros@gmail.com>
2021-11-08 13:51:38 +0100

    8/11/21

---
+++ 1EVA/EJERCICIOS/Arrays U4/ARRAYS_U4/4_1_1_1Ejercicio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ARRAYS_U4
+{
+    class Ejercicio_4_1_1_1
+    {
+        /*(4.1.1.1) Un programa que pida al usuario 4 números, los memorice (utilizando un 
+        array), calcule su media aritmética y después muestre en pantalla la media y los 
+        datos tecleados.*/
+        public static void Media()
+        {
+            int[] numeros = new int[4];
+            int media;
+
+            Console.WriteLine("Hola marinero. Introduce 4 números y hagamos magia juntos ;)");
+
+            numeros[0] = Convert.ToInt32(Console.ReadLine());
+            numeros[1] = Convert.ToInt32(Console.ReadLine());
+            numeros[2] = Convert.ToInt32(Console.ReadLine());
+            numeros[3] = Convert.ToInt32(Console.ReadLine());
+
+            media = (numeros[0] + numeros[1] + numeros[2] + numeros[3]) / 4;
+
+            Console.WriteLine("La media de {0}, {1}, {2}, y {3} es: {4}", numeros[0], numeros[1], numeros[2], numeros[3], media);
+        }
+    }
+}
