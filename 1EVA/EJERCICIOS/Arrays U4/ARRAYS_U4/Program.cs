beb924d5ce1ba218a77ccb22ba0de6e8cf1dfb35
mañana <jotacaparros@gmail.com>
2021-11-08 12:14:57 +0100

    08/11/21

---
+++ 1EVA/EJERCICIOS/Arrays U4/ARRAYS_U4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ARRAYS_U4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
