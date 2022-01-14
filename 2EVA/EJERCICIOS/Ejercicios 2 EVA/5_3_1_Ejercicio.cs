d446c223dc7a54bd03f2fa6616ddde56c8daff4c
mañana <jotacaparros@gmail.com>
2022-01-14 13:23:01 +0100

    14/01/22

--- 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_3_1_Ejercicio.cs
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_3_1_Ejercicio.cs
@@ -5,14 +5,33 @@ public class Ejercicio_5_3_1
 	/*(5.3.1) Crea una función "DibujarCuadrado" que dibuje en pantalla un cuadrado 
 	del ancho (y alto) que se indique como parámetro. Completa el programa con un 
 	Main que permita probarla.*/
+	static int ancho, alto;
 	public static void xxx()
 	{
+		Parametrea();
 		DibujarCuadrado();
 	}
 
+	public static void Parametrea()
+    {
+		Console.WriteLine("¿De qué tamaño quieres el cuadrado campeón?");
+        Console.Write("Dame el ancho:");
+		ancho = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Dame el alto:");
+		alto = Convert.ToInt32(Console.ReadLine());
+		
+	}
+	
 	public static void DibujarCuadrado()
     {
-
+		for (int i = 0; i < alto; i++)
+        {
+			for (int j = 0; j < ancho; j++)
+            {
+				Console.Write("*");
+            }
+			Console.WriteLine();
+        }
     }
 
 
---
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_3_2_Ejercicio.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Ejercicio_5_3_2
+{
+	/*(5.3.2) Crea una función "DibujarRectangulo" que dibuje en pantalla un rectángulo 
+	del ancho y alto que se indiquen como parámetros. Incluye un Main para probarla.*/
+	static int ancho, alto;
+	static bool salir = false;
+	public static void xxx()
+	{
+		do
+		{
+			Parametrea();
+			DibujarRectangulo();
+		} while (salir == false);
+	}
+
+	public static void Parametrea()
+    {
+		Console.WriteLine("¿De qué tamaño quieres el rectángulo campeón?");
+        Console.Write("Dame el ancho:");
+		ancho = Convert.ToInt32(Console.ReadLine());
+		Console.Write("Dame el alto:");
+		alto = Convert.ToInt32(Console.ReadLine());
+		
+	}
+	
+	public static void DibujarRectangulo()
+    {
+		if (alto != ancho)
+        {
+			for (int i = 0; i < alto; i++)
+			{
+				for (int j = 0; j < ancho; j++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			salir = true;
+        }
+        else
+        {
+            Console.WriteLine("El ancho y el alto no puede ser el mismo crack porque sino sería un cuadrado. Prueba otra vez máquina.\n");
+		
+        }
+    }
+
+
+}
