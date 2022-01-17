f089bfd1aed8b6bc865a66bd4df9357140b66663
mañana <jotacaparros@gmail.com>
2022-01-17 13:47:26 +0100

    17/01/22

--- 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_4_1_Ejercicio.cs
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_4_1_Ejercicio.cs
@@ -5,7 +5,18 @@ public class Ejercicio_5_4_1
 	/*(5.4.1) Crea una función "Cubo" que calcule el cubo de un número real (float) que se indique como parámetro. 
 	El resultado deberá ser otro número real. Prueba esta función para calcular el cubo de 3.2 y el de 5. */
 	
-	public Class1()
+	static float resultado; 
+	public static void xxx()
 	{
+        Console.WriteLine("Introduce un número que quieras saber su cubo.");
+		float numero = float.Parse(Console.ReadLine());
+		Cubo(ref numero);
+        Console.WriteLine(resultado);
 	}
+
+	public static float Cubo(ref float numero)
+    {
+		resultado = numero * numero * numero;
+		return resultado;
+    }
 }
---
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_4_2_Ejercicio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Ejercicio_5_4_2
+{
+	/*(5.4.2) Crea una función "Menor" que calcule el menor de dos números enteros que recibirá como parámetros. El resultado será otro número entero.*/
+	
+	public static void Main()
+	{
+		int parametro1, parametro2;
+        Console.WriteLine("Dame dos números y te digo cúal es el menor.");
+
+		parametro1 = Convert.ToInt32(Console.ReadLine());
+		parametro2 = Convert.ToInt32(Console.ReadLine());
+
+		Menor(ref parametro1, ref parametro2);
+	}
+
+	public static int Menor(int x, int y)
+    {
+		if (parametro1 > parametro2)
+        {
+			return y;
+        }else
+        {
+			return x;
+        }
+    }
+}
