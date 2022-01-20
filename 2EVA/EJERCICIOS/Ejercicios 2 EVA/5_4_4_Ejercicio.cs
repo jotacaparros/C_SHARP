8bb70ebd0e742a5be40c1be314900abea7a2588e
mañana <jotacaparros@gmail.com>
2022-01-20 11:01:25 +0100

    20/01/22

---
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_4_4_Ejercicio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Ejercicio_5_4_4
+{
+	/*(5.4.4) Crea una función "Inicial", que devuelva la primera letra de una cadena de texto.
+	 * Prueba esta función para calcular la primera letra de la frase "Hola".*/
+	public static void xxx()
+	{
+        Console.WriteLine("Escribe una frase y te doy la inicial caballero:");
+		string frase = Console.ReadLine();
+        Console.WriteLine(Inicial(frase));
+	}
+
+	public static char Inicial(string x)
+    {
+		char letraInicial = x[0];
+		return letraInicial;
+    }
+}
---
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_4_5_Ejercicio.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Ejercicio_5_4_5
+{
+	/*(5.4.5) Crea una función "UltimaLetra", que devuelva la última letra de una cadena de texto.
+	 * Prueba esta función para calcular la última letra de la frase "Hola".*/
+	public static void  ()
+	{
+	}
+}
