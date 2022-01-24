7f5a2c2a9ee8d9bef783ff52fd29b246003e4c87
Jota Caparrós <jotacaparros@gmail.com>
2022-01-24 14:03:34 +0100

    24/01/22

--- 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_5_1_Ejercicio.cs
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_5_1_Ejercicio.cs
@@ -2,16 +2,54 @@
 
 public class Ejercicio_5_5_1
 {
-	/*(5.5.1) Crea una función "PedirEntero", que reciba como parámetros el texto que se debe mostrar en pantalla, el valor mínimo aceptable
-	y el valor máximo aceptable. Deberá pedir al usuario que introduzca el valor tantas veces como sea necesario, volvérselo a pedir en caso 
-	de error, y devolver un valor correcto. Pruébalo con un programa que pida al usuario un año entre 1800 y 2100.*/
-	public static void Main()
+	/*(5.5.1) Crea una función "PedirEntero", que reciba como parámetros el texto
+	que se debe mostrar en pantalla, el valor mínimo aceptable y el valor máximo aceptable.
+	Deberá pedir al usuario que introduzca el valor tantas veces como sea necesario,
+	volvérselo a pedir en caso de error, y devolver un valor correcto. Pruébalo con
+	un programa que pida al usuario un año entre 1800 y 2100.*/
+
+	public static void xxx()
 	{
+		bool salir = true;
+		do
+		{
+			Console.WriteLine("Adivina el número que quiero:");
+			int numero = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PedirEntero(numero));
+			salir = Salir(numero);
+
+		} while (salir!);
 
 	}
 
-	public static void PedirEntero()
+	public static string PedirEntero(int x)
     {
+		
+		if (x < 1800 || x > 2100)
+        {
+			string fallaste = "Ese número me vale verga, prueba otra vez";
+			return fallaste;
+        }
+        else
+        {
+			string acierto = "GRAAANDE MAKINON";
+			return acierto; 
+        }
 
     }
+
+	public static bool Salir(int x)
+    {
+		if (x < 1800 || x > 2100)
+		{
+			bool noSale = true;
+			return noSale;
+		}
+		else
+		{
+			bool siSale = false;
+			return siSale;
+		}
+
+	}
 }
---
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/5_5_3_Ejercicio.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Ejercicios_2_EVA
+{
+    public class Ejercicio_5_5_3
+    {
+        /*(5.5.3) Crea una función "EsPrimo", que reciba un número y devuelva el valor booleano
+        "true" si es un número primo o "false" en caso contrario.*/
+
+        public static void xxx()
+        {
+            bool primo;
+            Console.WriteLine("Número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+            primo = EsPrimo(numero);
+            if (primo == true)
+            {
+                Console.WriteLine("Es primo");
+            }else
+            {
+                Console.WriteLine("No es primo");
+            }
+
+        }
+
+        public static bool EsPrimo(int x)
+        {
+            int a = 0;
+            for( int i= 1; i<= x; i++)
+            {
+                if(x % i == 0)
+                {
+                    a++;
+                }
+            }
+            if (a == 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
