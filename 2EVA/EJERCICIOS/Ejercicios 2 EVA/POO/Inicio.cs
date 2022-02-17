45da3700df5a24563f58d175b645a204cda8245f
mañana <jotacaparros@gmail.com>
2022-02-17 10:22:34 +0100

    17/02/22

--- 2EVA/EJERCICIOS/Ejercicios 2 EVA/POO/Inicio.cs
+++ 2EVA/EJERCICIOS/Ejercicios 2 EVA/POO/Inicio.cs
@@ -4,19 +4,7 @@ public class Inicio
 {
 	public static void Main()
 	{
-		Libro manuscrito1 = new Libro();
-
-		manuscrito1.SetAutor("Midas Fachero");
-		manuscrito1.SetTitulo("Cómo hacer un buen frances invertido y llegar al orgasmo");
-		manuscrito1.SetPaginas(895);
-		manuscrito1.SetUbicacion("Toledo");
-
-        Console.WriteLine(manuscrito1.GetTitulo());
-        Console.WriteLine(manuscrito1.GetAutor()); 
-        Console.WriteLine(manuscrito1.GetPaginas());
-        Console.WriteLine(manuscrito1.GetUbicacion()); 
-		
-
+	
 	
 
 	}
