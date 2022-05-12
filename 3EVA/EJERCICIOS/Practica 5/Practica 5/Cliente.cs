66ed241c59e468d4712b290159c978a4d71f21b3
mañana <jotacaparros@gmail.com>
2022-05-12 10:46:52 +0200

    12/05/22

---
+++ 3EVA/EJERCICIOS/Practica 5/Practica 5/Cliente.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class Cliente: Persona
+{
+	public Cliente()
+	{
+	}
+}
---
+++ 3EVA/EJERCICIOS/Practica 5/Practica 5/Persona.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Persona
+{
+	public Persona()
+	{
+		string nombre;
+		string apellido;
+		string provincia;
+	}
+}
